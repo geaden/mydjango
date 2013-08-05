0993a5f5201b2ffa34ebfac2efab1c807b07b4d1
Gennady Denisov <denisovgena@gmail.com>
2013-08-05 21:39:52 +0400

    fix typo again. Add media url

--- mydjango/mydjango/settings/test.py
+++ mydjango/mydjango/settings/test.py
@@ -9,7 +9,7 @@ run test suite locally
 TEST_RUNNER = 'discover_runner.DiscoverRunner'
 TEST_DISCOVER_TOP_LEVEL = PROJECT_ROOT
 TEST_DISCOVER_ROOT = PROJECT_ROOT
-TEST_DISCOER_PATTERN = 'test_*'
+TEST_DISCOVER_PATTERN = 'test_*'
 
 
 DATABASES = {
